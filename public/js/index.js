d50a830521e722ac7dfe5a971ea9bd527866bbca
typable <typable@prototype-studio.de>
2020-05-16 00:22:47 +0200

    Sanitized client JavaScript

--- public/js/index.js
+++ public/js/index.js
@@ -1,192 +1,184 @@
-// define
+let env = {
+	drag: false,
+	last: null,
+	color: 'black',
+	radius: 3,
+	bucket: [
+		{ name: 'Black', code: 'black', active: true },
+		{ name: 'White', code: 'white' },
+		{ name: 'Red', code: 'red' },
+		{ name: 'Blue', code: 'blue' },
+		{ name: 'Green', code: 'green' },
+		{ name: 'Yellow', code: 'yellow' },
+		{ name: 'Brown', code: 'brown' },
+		{ name: 'Grey', code: 'grey' },
+		{ name: 'Purple', code: 'purple' },
+		{ name: 'Bisque', code: 'bisque' }
+	],
+	socket: null
+};
+
 let canvas;
 let g;
-let drag;
-let last;
-let radius = 3;
-let socket;
-let color = 'black';
-let ip = location.host; // '127.0.0.1';
 
-const Player = {
-	name: null,
-	ip: null
-};
+window.addEventListener('load', function(event) {
 
-const Channel = {
-	name: null
-};
-
-let buckets = [
-	{ name: 'Black', code: 'black', active: true },
-	{ name: 'White', code: 'white' },
-	{ name: 'Red', code: 'red' },
-	{ name: 'Blue', code: 'blue' },
-	{ name: 'Green', code: 'green' },
-	{ name: 'Yellow', code: 'yellow' },
-	{ name: 'Brown', code: 'brown' },
-	{ name: 'Grey', code: 'grey' },
-	{ name: 'Purple', code: 'purple' },
-	{ name: 'Bisque', code: 'bisque' }
-];
-
-// onload
-window.onload = e => {
-	// init
-	canvas = document.querySelector('#canvas');
+	canvas = document.querySelector('.canvas');
 	canvas.draggable = true;
+
 	g = canvas.getContext('2d');
-	// events
-	canvas.ondragstart = e => false;
-	canvas.oncontextmenu = e => false;
-	canvas.onmousedown = e => {
-		drag = true;
-		let pos = { x: e.layerX, y: e.layerY };
-		send({ pos: pos, last: last });
-		draw(pos, last, radius, color);
-		last = pos;
-	};
-	document.onmouseup = e => {
-		drag = false;
-		last = null;
-	};
-	canvas.onmousemove = e => {
-		if(drag) {
-			let pos = { x: e.layerX, y: e.layerY };
-			send({ pos: pos, last: last });
-			draw(pos, last, radius, color);
-			last = pos;
+
+	env.bucket.forEach(function(item) {
+		let template = document.createElement('template');
+		template.innerHTML = `<div class="bucket tooltip up${item.active ? ' focused' : ''}" data-title="${item.name}" style="background: ${item.code}"></div>`;
+		document.querySelector('.toolbar').append(template.content.cloneNode(true));
+	});
+
+	canvas.addEventListener('dragstart', function(event) {
+		event.preventDefault();
+	});
+	canvas.addEventListener('contextmenu', function(event) {
+		event.preventDefault();
+	});
+	canvas.addEventListener('mousemove', function(event) {
+		if(env.drag) {
+			let pos = { x: event.layerX, y: event.layerY };
+			send({ pos: pos, last: env.last });
+			draw(pos, env.last, env.radius, env.color);
+			env.last = pos;
 		}
-	};
-	document.onwheel = e => {
-		if(e.deltaY < 0) {
-			radius += radius < 30 ? 0.5 : 0;
+	});
+	canvas.addEventListener('mousedown', function(event) {
+		env.drag = true;
+		let pos = { x: event.layerX, y: event.layerY };
+		send({ pos: pos, last: env.last });
+		draw(pos, env.last, env.radius, env.color);
+		env.last = pos;
+	});
+
+	document.addEventListener('mouseup', function(event) {
+		env.drag = false;
+		env.last = null;
+	});
+	document.addEventListener('wheel', function(event) {
+
+		if(event.deltaY < 0) {
+			env.radius += env.radius < 30 ? 0.5 : 0;
 		}
 		else {
-			radius -= radius > 0.5 ? 0.5 : 0;
+			env.radius -= env.radius > 0.5 ? 0.5 : 0;
 		}
-	};
-	document.onkeydown = e => {
-		if(e.keyCode == 9) {
-			// HUD fade in
-		}
-		if(e.keyCode >= 48 && e.keyCode <= 57) {
-			let index = e.keyCode - 49;
+	});
+	document.addEventListener('keydown', function(event) {
+
+		if(event.keyCode >= 48 && event.keyCode <= 57) {
+			let index = event.keyCode - 49;
 			if(index == -1) {
 				index = 9;
 			}
-			Array.from(document.querySelectorAll('.bucket')).forEach((bucket, i) => {
-				if(index == i) {
-					color = bucket.style.background;
-					Array.from(document.querySelectorAll('.bucket')).forEach(b => {
-						if(b != bucket) {
-							b.classList.remove('focused');
+			Array.from(document.querySelectorAll('.bucket'))
+				.forEach(function(item, i) {
+					if(index == i) {
+						env.color = item.style.background;
+						item.classList.add('focused');
+					}
+					else {
+						item.classList.remove('focused');
+					}
+				});
+		}
+	});
+
+	window.addEventListener('resize', resize);
+
+	Array.from(document.querySelectorAll('div.bucket'))
+		.forEach(function(item) {
+			item.addEventListener('click', function(event) {
+				env.color = item.style.background;
+				Array.from(document.querySelectorAll('.bucket'))
+					.forEach(function(_item) {
+						if(_item != item) {
+							_item.classList.remove('focused');
 						}
 					});
-					bucket.classList.add('focused');
-				}
+				item.classList.add('focused');
 			});
-		}
-	};
-	document.onkeyup = e => {
-		if(e.keyCode == 9) {
-			// HUD fade out
-		}
-	};
-	window.onresize = e => resize();
-	// ready
+		});
+
+	document.querySelector('button.action-clear')
+		.addEventListener('click', function(event) {
+			env.socket.emit('clear');
+			g.clearRect(0, 0, window.innerWidth, window.innerHeight);
+		});
+
+	document.querySelector('button.action-background')
+		.addEventListener('click', function(event) {
+			env.socket.emit('background', env.color);
+			g.fillStyle = env.color;
+			g.fillRect(0, 0, window.innerWidth, window.innerHeight);
+			g.fillStyle = 'black';
+		});
+
 	resize();
 	connect();
+});
 
-	buckets.forEach(bucket => {
-		let tag = document.createElement('div');
-		tag.classList.add('bucket');
-		tag.classList.add('tooltip');
-		tag.classList.add('up');
-		tag.setAttribute('data-title', bucket.name);
-		tag.style.background = bucket.code;
-		if(bucket.active) {
-			tag.classList.add('focused');
-		}
-		tag.addEventListener('click', e => {
-			color = tag.style.background;
-			Array.from(document.querySelectorAll('.bucket')).forEach(b => {
-				if(b != bucket) {
-					b.classList.remove('focused');
-				}
-			});
-			tag.classList.add('focused');
-		});
-		document.querySelector('.toolbar').append(tag);
-	});
-
-	document.querySelector('#clear').addEventListener('click', e => {
-		socket.emit('clear');
-		g.clearRect(0, 0, window.innerWidth, window.innerHeight);
-	});
-
-	document.querySelector('#background').addEventListener('click', e => {
-		socket.emit('background', color);
-		g.fillStyle = color;
-		g.fillRect(0, 0, window.innerWidth, window.innerHeight);
-		g.fillStyle = 'black';
-	});
-};
-
-// functions
-let resize = () => {
+function resize() {
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
-};
-let connect = () => {
-	socket = io('http://' + ip, {
+}
+
+function connect() {
+	env.socket = io('http://' + window.location.host, {
 		path: '/pipe'
 	});
-	socket.on('data', snap => {
-		draw(snap.pos, snap.last, snap.rad, snap.clr);
+	env.socket.on('data', function(data) {
+		draw(data.pos, data.last, data.radius, data.color);
 	});
-	socket.on('clear', snap => {
+	env.socket.on('clear', function(data) {
 		g.clearRect(0, 0, window.innerWidth, window.innerHeight);
 	});
-	socket.on('background', snap => {
-		g.fillStyle = snap;
+	env.socket.on('background', function(data) {
+		g.fillStyle = data;
 		g.fillRect(0, 0, window.innerWidth, window.innerHeight);
 		g.fillStyle = 'black';
 	});
-};
-let send = ({ pos, last }) => {
-	if(socket) {
-		socket.emit('data', {
+}
+
+function send({ pos, last }) {
+	if(env.socket) {
+		env.socket.emit('data', {
 			pos: pos,
 			last: last,
-			rad: radius,
-			clr: color
+			radius: env.radius,
+			color: env.color
 		});
 	}
-};
-let draw = (pos, last, rad, clr) => {
+}
+
+function draw(pos, last, radius, color) {
 	if(last) {
 		let diff = {
 			x: last.x - pos.x,
 			y: last.y - pos.y
 		};
 		let dist = Math.sqrt(Math.pow(diff.x, 2) + Math.pow(diff.y, 2), 2);
-		let length = dist / (rad / 2);
+		let length = dist / (radius / 2);
 		for(let i = 0; i < length; i++) {
 			let x = pos.x + (diff.x / length * i);
 			let y = pos.y + (diff.y / length * i);
-			g.fillStyle = clr;
+			g.fillStyle = color;
 			g.beginPath();
-			g.arc(x, y, 2 * rad, 0, 2 * Math.PI);
+			g.arc(x, y, 2 * radius, 0, 2 * Math.PI);
 			g.fill();
 			g.fillStyle = 'black';
 		}
 	}
 	else {
-		g.fillStyle = clr;
+		g.fillStyle = color;
 		g.beginPath();
-		g.arc(pos.x, pos.y, 2 * rad, 0, 2 * Math.PI);
+		g.arc(pos.x, pos.y, 2 * radius, 0, 2 * Math.PI);
 		g.fill();
 		g.fillStyle = 'black';
 	}
-};
+}
