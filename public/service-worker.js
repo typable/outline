2051d08b476220e80150d2e85648bfb77e1777f8
typable <typable@prototype-studio.de>
2020-08-26 11:29:24 +0200

    Added offline support

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,27 @@
-const FILES_TO_CACHE = [];
+const FILES_TO_CACHE = [
+	'/',
+	'/index.html',
+	'/manifest.json',
+	'/asset/img/favicon.png',
+	'/asset/img/app/icon-192x192.png',
+	'/asset/lang/en.properties',
+	'/asset/lang/de.properties',
+	'/asset/font/material-icons.woff2',
+	'/asset/font/roboto-thin.ttf',
+	'/asset/font/roboto-light.ttf',
+	'/asset/font/roboto-regular.ttf',
+	'/asset/font/roboto-medium.ttf',
+	'/asset/font/roboto-bold.ttf',
+	'/asset/font/roboto-black.ttf',
+	'/css/style.css',
+	'/js/index.js',
+	'/js/constant.js',
+	'/js/util.js',
+	'/js/addon/canvas.js',
+	'/js/addon/locale.js',
+	'/js/addon/ripple.js',
+	'/js/addon/gamepad.js'
+];
 
 self.addEventListener('install', function(event) {
 	event.waitUntil(
